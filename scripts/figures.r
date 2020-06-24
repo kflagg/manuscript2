d90c401ff40ca906e15907ce76acca84e4e3bf59
Kenny Flagg <kenneth.flagg@msu.montana.edu>
2020-06-23 21:06:16 -0600

    Plotted example designs.

--- scripts/figures.r
+++ scripts/figures.r
@@ -33,6 +33,25 @@ rect_datasets <- readRDS('rect_data.rds')
 
 allplans <- readRDS('rect_plans.rds')
 
+plotplans <- c(
+  'Hilbert000180',
+  'Inhib000171',
+  'LHS-TSP000259',
+  'RPM001107',
+  'Serp000277',
+  'SRS000214',
+  'Sys000013'
+)
+
+for(planid in plotplans){
+  thisplan <- allplans %>% filter(PlanID == planid)
+  pdf(paste0('../writeup/', planid, '.pdf'), width = 9, height = 4)
+  par(mar = c(0, 0, 2, 0))
+  plot(thisplan$Plan[[1]], main = paste(thisplan$Scheme, 'Example'))
+  plot(rect_R, border = 'grey', add = TRUE)
+  dev.off()
+}
+
 # Create combinations of plans and data.
 #fit_design <- expand.grid(PlanID = allplans$PlanID, DataID = rect_datasets$DataID)
 
